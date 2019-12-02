18d907f51de90d79295253afddf41dfb90b448b5
jvsena42 <jvsena16@gmail.com>
2019-12-01 21:49:36 -0300

    Login do usuário

--- app/src/main/java/com/app/instagram/activity/CadastroActivity.java
+++ app/src/main/java/com/app/instagram/activity/CadastroActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +19,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
--- app/src/main/java/com/app/instagram/activity/LoginActivity.java
+++ app/src/main/java/com/app/instagram/activity/LoginActivity.java
@@ -1,23 +1,102 @@
 package com.app.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.app.instagram.R;
+import com.app.instagram.helper.ConfiguracaoFirebase;
+import com.app.instagram.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail,campoSenha;
+    private Button botaoEntrar;
+    private ProgressBar progressBar;
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        verificarUsuarioLogado();
+        inicializarComponentes();
+
+        //Fazer Login do usuário
+        progressBar.setVisibility(View.GONE);
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if (!textoEmail.isEmpty() && !textoSenha.isEmpty()){
+                    usuario = new Usuario();
+                    usuario.setEmail(textoEmail);
+                    usuario.setSenha(textoSenha);
+                    validarLogin(usuario);
+
+                }else {
+                    Toast.makeText(LoginActivity.this,"Preencha todos os campos!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getReferenciaAutenticacao();
+        if (autenticacao.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
     }
 
     public void abrirCadastro (View view){
         Intent i = new Intent(LoginActivity.this,CadastroActivity.class);
         startActivity(i);
+    }
+
+    public void validarLogin(Usuario usuario){
+        progressBar.setVisibility(View.VISIBLE);
+        autenticacao = ConfiguracaoFirebase.getReferenciaAutenticacao();
+
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(),usuario.getSenha())
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressBar.setVisibility(View.GONE);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this,"Erro ao fazer login!", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+    public void inicializarComponentes(){
+        campoEmail = findViewById(R.id.editLoginEmail);
+        campoSenha = findViewById(R.id.editLoginSenha);
+        botaoEntrar = findViewById(R.id.buttonEntrar);
+        progressBar = findViewById(R.id.progressLogin);
+
+        campoEmail.requestFocus();
     }
 }
