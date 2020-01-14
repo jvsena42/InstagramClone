2d364b13e140b0ffb086252a0e5d59915c915ed6
jvsena42 <jvsena16@gmail.com>
2020-01-14 20:53:45 -0300

    Criando interfaces de filtros

--- app/src/main/java/com/app/instagram/activity/FiltroActivity.java
+++ app/src/main/java/com/app/instagram/activity/FiltroActivity.java
@@ -1,10 +1,14 @@
 package com.app.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.app.instagram.R;
@@ -22,6 +26,15 @@ public class FiltroActivity extends AppCompatActivity {
         //Inicializar componentes
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
 
+        //Configurar Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        toolbar.setTitleTextColor( getResources().getColor(R.color.colorPrimaryDark));
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
         //Recuperar foto escolhida pelo usuário
         Bundle bundle = getIntent().getExtras();
         if (bundle != null){
@@ -29,5 +42,32 @@ public class FiltroActivity extends AppCompatActivity {
             imagem = BitmapFactory.decodeByteArray(dadosImagem,0,dadosImagem.length);
             imageFotoEscolhida.setImageBitmap(imagem);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.ic_salvar_postagem:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
